b24aca87a133c12d8eac86b426f4d61cbf8ce93c
Turbadrakh <turbadrah612@gmail.com>
2026-04-29 21:28:29 +0200

    cors oorchlov2

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,21 @@
 import express from "express";
 import cors from "cors";
 
-app.use(
-  cors({
-    origin: "https://oe-market.vercel.app",
-    credentials: true,
-  }),
-);
-const app = express(); //create an Express app
+const app = express();
+
+app.use(cors()); // ✅ THIS FIXES YOUR ERROR
 
 app.use(express.json());
 
-//route import
+// routes
 import userRouter from "./routes/user.routes.js";
 import listingRouter from "./routes/listing.routes.js";
 import savedRouter from "./routes/saved.routes.js";
 
-//routes declaration
 app.use("/api/v1/saved", savedRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/listings", listingRouter);
+
 app.use("/uploads", express.static("backend/uploads"));
-//example route: http://localhost:4000/api/v1/users/register
 
 export default app;
