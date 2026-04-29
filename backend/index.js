921d97086be4dacb8af50151e60c0c5d77309f29
Turbadrakh <turbadrah612@gmail.com>
2026-04-29 19:49:56 +0200

    debug logs

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,23 @@
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
 import app from "./app.js";
+
 dotenv.config();
+
+console.log("STARTING SERVER...");
+console.log("MONGO:", process.env.MONGODB_URI);
 
 const startServer = async () => {
   try {
     await connectDB();
 
-    app.on("error", (error) => {
-      console.log("ERROR", error);
-      throw error;
-    });
     const PORT = process.env.PORT || 8000;
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`SERVER IS RUNNING ON PORT : ${PORT}`);
+
+    app.listen(PORT, () => {
+      console.log(`SERVER RUNNING ON ${PORT}`);
     });
   } catch (error) {
-    console.log("MONGODB CONNECTION FAILED", error);
+    console.log("ERROR:", error);
   }
 };
 
